0a0c10439622459b5f5465468f398d5fbe17b9fc
Richard Warburton <richard.warburton@gmail.com>
2015-06-11 16:03:03 +0100

    test out compilation of proper fix dictionaries

---
+++ src/test/java/uk/co/real_logic/fix_gateway/system_tests/FixCompilationTest.java
@@ -0,0 +1,28 @@
+package uk.co.real_logic.fix_gateway.system_tests;
+
+import org.junit.Test;
+import uk.co.real_logic.fix_gateway.dictionary.GenerationTool;
+
+public class FixCompilationTest
+{
+
+    @Test
+    public void shouldGenerateQuickFix44Dictionary() throws Exception
+    {
+        generateDictionary("src/test/resources/FIX44.xml");
+    }
+
+    @Test
+    public void shouldGenerateQuickFix42Dictionary() throws Exception
+    {
+        generateDictionary("src/test/resources/FIX42.xml");
+    }
+
+    private void generateDictionary(final String xmlPath) throws Exception
+    {
+        final String outputPath = "build/generated";
+        final String[] args = {outputPath, xmlPath};
+        GenerationTool.main(args);
+    }
+
+}
