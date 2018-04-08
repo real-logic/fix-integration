56f75e3a4679d0d50ba3d2210a60d03afe3add74
Alexey Utkin <utkin@devexperts.com>
2018-04-09 00:00:51 +0300

    FIX 5.0 support

---
+++ test-fix-50/src/test/java/uk/co/real_logic/artio/acceptance_tests/Fix50SpecAcceptanceTest.java
@@ -0,0 +1,149 @@
+package uk.co.real_logic.artio.acceptance_tests;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import uk.co.real_logic.artio.builder.LogonEncoder;
+import uk.co.real_logic.artio.builder.LogoutEncoder;
+import uk.co.real_logic.artio.session.SessionCustomisationStrategy;
+
+import java.nio.file.Path;
+import java.util.*;
+import java.util.function.Supplier;
+
+@RunWith(Parameterized.class)
+public class Fix50SpecAcceptanceTest extends AbstractFixSpecAcceptanceTest
+{
+    private static final String QUICKFIX_5_0_ROOT_PATH = QUICKFIX_DEFINITIONS + "/fix50";
+    private static final String CUSTOM_5_0_ROOT_PATH = CUSTOM_ROOT_PATH + "/fix50";
+
+    /**
+     * banned acceptance tests - not part of the spec we're aiming to support
+     */
+    private static final Set<String> BLACKLIST = new HashSet<>(Arrays.asList(
+        // TODO: ask for feedback on the following
+        // ignore if garbled, should we allow this, or just disconnect?
+        "2d_GarbledMessage.def",
+        "3c_GarbledMessage.def",
+
+        "2r_UnregisteredMsgType.def", // how do we validate/configure this?
+
+        "14i_RepeatingGroupCountNotEqual.def", // Is this required?
+        "14j_OutOfOrderRepeatingGroupMembers.def", // Is this required?
+
+        // Permanent Blacklist:
+
+        // These tests are all run as integration tests using validation
+        "14b_RequiredFieldMissing.def", // reject messages with required field missing
+        "14e_IncorrectEnumValue.def",
+        "14f_IncorrectDataFormat.def",
+        "14h_RepeatedTag.def",
+
+        // Refer to New Order Single, thus business domain validation.
+        "19a_PossResendMessageThatHAsAlreadyBeenSent.def",
+        "19b_PossResendMessageThatHasNotBeenSent.def",
+        // These tests make new order single behaviour assumptions, we have equivalent unit tests to these that don't
+        "2f_PossDupOrigSendingTimeTooHigh.def",
+        "2g_PossDupNoOrigSendingTime.def",
+
+        // Customers have asked for the opposite
+        "15_HeaderAndBodyFieldsOrderedDifferently.def",
+        "14g_HeaderBodyTrailerFieldsOutOfOrder.def"
+    ));
+
+    // Medium:
+    // "2m_BodyLengthValueNotCorrect.def" - length too short
+
+    // Low
+    // "8_AdminAndApplicationMessages.def"
+    // "8_OnlyAdminMessages.def"
+    // "8_OnlyApplicationMessages.def"
+
+    private static final List<String> QUICKFIX_WHITELIST = Arrays.asList(
+        "10_MsgSeqNumEqual.def",
+        "10_MsgSeqNumLess.def",
+        "1a_ValidLogonWithCorrectMsgSeqNum.def",
+        "1b_DuplicateIdentity.def",
+        "1c_InvalidTargetCompID.def",
+        "1c_InvalidSenderCompID.def",
+        // TODO: move this to a custom definition, for the modified error message
+        // "1d_InvalidLogonBadSendingTime.def",
+        "1d_InvalidLogonWrongBeginString.def",
+        "1d_InvalidLogonLengthInvalid.def",
+        "2a_MsgSeqNumCorrect.def",
+        "2c_MsgSeqNumTooLow.def",
+        "2e_PossDupAlreadyReceived.def",
+        "2e_PossDupNotReceived.def",
+        "2i_BeginStringValueUnexpected.def",
+        "2k_CompIDDoesNotMatchProfile.def",
+        "2o_SendingTimeValueOutOfRange.def",
+        "4a_NoDataSentDuringHeartBtInt.def",
+        "4b_ReceivedTestRequest.def",
+        "7_ReceiveRejectMessage.def",
+        "10_MsgSeqNumEqual.def",
+        "10_MsgSeqNumLess.def",
+        "11c_NewSeqNoLess.def",
+        "11a_NewSeqNoGreater.def",
+        "11b_NewSeqNoEqual.def",
+        "13b_UnsolicitedLogoutMessage.def",
+        "14c_TagNotDefinedForMsgType.def", // Tag not defined for this message type - add to set
+        "14d_TagSpecifiedWithoutValue.def", // Tag specified without a value - needs a check, second set
+        "QFJ648_NegativeHeartBtInt.def",
+        "QFJ650_MissingMsgSeqNum.def"
+    );
+
+    private static final List<String> CUSTOM_WHITELIST = Arrays.asList(
+        "14a_BadField.def", // reject messages with invalid field numbers
+        "1e_NotLogonMessage.def", // also has wrong target comp id
+//        // Edited logon at the end, sequence number looks invalid:
+        "2b_MsgSeqNumTooHigh.def",
+        "1a_ValidLogonMsgSeqNumTooHigh.def",
+        "2q_MsgTypeNotValid.def",
+//
+//        // Edited to make messages valid apart from first three fields
+        "2t_FirstThreeFieldsOutOfOrder.def",
+//
+        "10_MsgSeqNumGreater.def",  // Added reply to test request that looks valid
+        "6_SendTestRequest.def",    // Failing to pickup disconnect
+        "3b_InvalidChecksum.def"    // Modified to account for resend request with no NewOrderSingle
+    );
+
+    @Parameterized.Parameters(name = "Acceptance: {1}")
+    public static Collection<Object[]> data()
+    {
+        final List<Object[]> tests = new ArrayList<>();
+        tests.addAll(fix50Tests());
+        tests.addAll(fix50CustomisedTests());
+        return tests;
+    }
+
+    private static List<Object[]> fix50CustomisedTests()
+    {
+        return testsFor(CUSTOM_5_0_ROOT_PATH, CUSTOM_WHITELIST,
+            () -> Environment.fix50(new Fix5SessionCustomizationStrategy()));
+    }
+
+    private static List<Object[]> fix50Tests()
+    {
+        return testsFor(QUICKFIX_5_0_ROOT_PATH, QUICKFIX_WHITELIST,
+            () -> Environment.fix50(new Fix5SessionCustomizationStrategy()));
+    }
+
+    public Fix50SpecAcceptanceTest(
+        final Path path, final Path filename, final Supplier<Environment> environment)
+    {
+        super(path, filename, environment);
+    }
+
+    private static class Fix5SessionCustomizationStrategy implements SessionCustomisationStrategy {
+        @Override
+        public void configureLogon(LogonEncoder logon, long sessionId) {
+            logon.defaultApplVerID("7");
+        }
+
+        @Override
+        public void configureLogout(LogoutEncoder logout, long sessionId) {
+
+        }
+    }
+
+}
